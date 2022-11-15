ba9afdc76138cc436495947fd0856a2bbabd7a2d
Chandrasekhargollapalli <111641344+Chandrasekhargollapalli@users.noreply.github.com>
2022-11-15 13:12:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+let current =1 
+next.addEventListener('click', () => {
+    current++
+
+    if(current > circles.length){
+        current = circles.length
+        
+    }
+    chandu()
+})
+prev.addEventListener('click', () => {
+    current--
+    if(current <1 ) {
+current =1
+    }
+    chandu()
+})
+function chandu(){
+    circles.forEach((circle,idx) =>{
+        if(idx < current){
+            circle.classList.add('active')
+        }else {
+            circle.classList.remove('active')
+        }
+    })
+    const actives = document.querySelectorAll('.active')
+    progress.style.width =(actives.length - 1) / (circles.length -1) * 100 + '%'
+
+    if(current === 1){
+        prev.disabled = true
+    }else if(current === circles.length){
+        next.disabled = true
+    }else {
+        prev.disabled = false
+        next.disabled = false
+    }
+ }
